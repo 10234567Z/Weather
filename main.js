be46aafe758bb75199c69ea55112c5f70651f6d2
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-08-24 21:05:15 +0530

    Add an error message display on screen upon invalid location input

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-alert */
 
-const locName = "";
+let locName = "";
 async function GetWeather(url) {
   try {
     if (locName === null || locName.trim() === "") {
       throw new Error("Empty Location name submitted");
     }
     const response = await fetch(url, { mode: "cors" });
+    if (!response.ok) {
+      throw new Error("Not a valid location");
+    }
     const responseJson = await response.json();
-    console.log(responseJson);
   } catch (error) {
-    console.error(error);
+    document.querySelector(".errors").innerHTML = error.message;
   }
 }
+
+document.querySelector("input").addEventListener("change", (e) => {
+  locName = e.target.value;
+  GetWeather(`https://api.weatherapi.com/v1/forecast.json?key=a4c6435f1e1c492d88311257232208&q=${locName}&days=10&aqi=no&alerts=no`);
+});
+
+document.querySelector(".searchIco").addEventListener("mousedown", () => {
+  locName = document.querySelector("input").value;
+  GetWeather(`https://api.weatherapi.com/v1/forecast.json?key=a4c6435f1e1c492d88311257232208&q=${locName}&days=10&aqi=no&alerts=no`);
+});
