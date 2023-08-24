687c43e86c63be685fb6ce37cc64af3bc9b2523a
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-08-24 20:48:19 +0530

    Add basic layout for the webpage

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-alert */
-const locName = prompt("Enter the location", "");
 
+const locName = "";
 async function GetWeather(url) {
   try {
     if (locName === null || locName.trim() === "") {
@@ -8,11 +9,8 @@ async function GetWeather(url) {
     }
     const response = await fetch(url, { mode: "cors" });
     const responseJson = await response.json();
-    document.querySelector(".location").innerHTML = JSON.stringify(responseJson.location);
-    document.querySelector(".currentWeather").innerHTML = JSON.stringify(responseJson.current);
+    console.log(responseJson);
   } catch (error) {
     console.error(error);
   }
 }
-
-GetWeather(`http://api.weatherapi.com/v1/current.json?key=a4c6435f1e1c492d88311257232208&q=${locName}`);
